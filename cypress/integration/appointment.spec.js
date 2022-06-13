bc2b04c0396a6e178dcbe62811aebb098eeb3b1e
Manuel Casanova <manucasanova@hotmail.com>
2022-06-13 13:29:58 -0700

    Add tests for add interview@

---
+++ cypress/integration/appointment.spec.js
@@ -0,0 +1,35 @@
+describe("Appointments", () => {
+
+  it("should book an interview", () => {
+
+
+    //Resets the DB to the beginning of the test.
+    cy.request("GET", "http://localhost:8001/api/debug/reset")
+
+    cy.visit("/");
+
+    cy.contains("Monday");
+
+
+    //Find add button and click on it.
+    cy.get("[alt=Add]")
+ .first()
+ .click();
+
+    //Get input field and type name
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones")
+
+
+    //Select interviewer
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    //Finds the button and clicks
+    cy.contains("Save").click();
+
+    
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+
+
+  });
+}); 
