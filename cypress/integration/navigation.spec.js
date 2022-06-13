87df2ecf383612c988ca837242e2433c267ee03c
Manuel Casanova <manucasanova@hotmail.com>
2022-06-13 12:38:31 -0700

    Refactor navigating day tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,12 +4,8 @@ describe("Navigation", () => {
   });
 
   it("should navigate to Tuesday", () => {
-    cy.visit("/");
-
-    cy.get("li").contains("Tuesday").click();
-
-    cy.get("li")
-  .contains("li", "Tuesday")
-  .should("have.css", "background-color", "rgb(242, 242, 242)");
+    
+  cy.contains("li", "Tuesday").click().should("have.css", "background-color", "rgb(242, 242, 242)");
+  
   });
-});
+}); 
